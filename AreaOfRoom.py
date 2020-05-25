e4c1a5bf082fd1c9cd5ad4b8e82129125749ef58
Gulshan Kumar Verma <33981412+scorpion231@users.noreply.github.com>
2020-05-25 18:43:07 +0530

    Program to calculate area of the room

---
+++ AreaOfRoom.py
@@ -0,0 +1,18 @@
+##
+#Compute the area of the room
+#
+
+#Read the dimensions from the user
+
+l=float(input("Enter the length of the room in feet : "))
+b=float(input("Enter the breadth of the room in feet : "))
+
+
+#compute the area of the room
+
+a=l*b
+
+
+#display
+
+print("The area of the room is ",a," Square feet")
